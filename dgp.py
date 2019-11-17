109940b403bfb31aadb92d73e75188fce8741590
Harsh Parikh <harsh081193@gmail.com>
2019-11-17 13:26:24 -0500

    dgp updated

---
+++ dgp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 17 13:13:43 2019
+
+@author: harshparikh
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-darkgrid')
+
+# Initialize random number generator
+np.random.seed(123)
+
+# True parameter values
+alpha, sigma = 1, 1
+beta = [1, 2.5]
+
+# Size of dataset
+size = 100
+
+# Predictor variable
+X1 = np.random.randn(size)
+X2 = np.random.randn(size) * 0.2
+
+# Simulate outcome variable
+Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
+
+fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10,4))
+axes[0].scatter(X1, Y)
+axes[1].scatter(X2, Y)
+axes[0].set_ylabel('Y'); axes[0].set_xlabel('X1'); axes[1].set_xlabel('X2');
+fig.savefig('dgp_standard.png')
